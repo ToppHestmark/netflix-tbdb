5853cae3b8ae289b814d33a1052f3117b73e5600
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-06 23:00:45 +0100

    Added YouTube trailer for the posters

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # NETFLIX
 
 Cloning netflix with TMDb API data.
+Illustrating the banners as Netflix and show YouTube trailer of the selected poster onClick if url is available on the selected poster.
 
-Dependencies
+Dependencies included
 - Axios
 - Styled components
+- react-youtube
+- movie-trailer
--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../../utils/axios';
+import YouTube from 'react-youtube';
+import movieTrailer from "movie-trailer";
 import { Container, Title, Posters, Poster } from './styles/row';
 
 const posters_base_url =  "https://image.tmdb.org/t/p/original/";
 
 export default function Row({ title, fetchURL, isLargerRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     useEffect(() => {
         async function fetchData() {
@@ -15,12 +18,36 @@ export default function Row({ title, fetchURL, isLargerRow }) {
                 return request;
             }
             catch(err) {
-                console.log(err);
+                console.log(err.message);
             }
         }
         fetchData()
     }, [fetchURL]);
 
+    // ----- react-youtube properties -----
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+          // http://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        }
+      };
+
+    const getTrailer = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer( movie?.title || "" )
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"))
+            })
+            .catch(err => console.log(err.message))
+        }
+        return trailerUrl;
+    };
+    
     return (
         <Container>
             <Title> {title} </Title>
@@ -31,9 +58,11 @@ export default function Row({ title, fetchURL, isLargerRow }) {
                         key={movie.id}
                         src={`${posters_base_url}${isLargerRow ? movie.poster_path : movie.backdrop_path}`}
                         alt={movie.title}
+                        onClick={() => getTrailer(movie)}
                     />
                 ))}
             </Posters>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </Container>
     )
 }
